76b5f37bd8fb5fcb58f9b803bab213f7fccc7758
jesseDtucker <jesseDtucker@gmail.com>
2014-11-05 14:50:24 -0700

    No more copies, also conforms to Miller algorithm

--- lib/parallel_merge_sort.rb
+++ lib/parallel_merge_sort.rb
@@ -12,43 +12,48 @@ module ParallelMergeSort
   end
 
   private
-  def self.sortItems(items, comparator)
-    return items if items.length == 1
-
-    left = []
-    right = []
+  def self.sortItems(items, comparator, left=0, right=items.length)
+    return unless right > left
     
+    mid = (left + right) / 2
     t1 = Thread.new do ||
-      left = sortItems(items.slice(0, items.length / 2), comparator).reverse
+      sortItems(items, comparator, left, mid)
     end
-    right = sortItems(items.slice(items.length / 2, items.length), comparator).reverse
+    t2 = Thread.new do ||
+      sortItems(items, comparator, mid + 1, right)
+    end
     
     t1.join
+    t2.join
     
-    items.map! do ||
-      a = left.last
-      b = right.last
+    leftStart = left
+    leftEnd = mid
+    rightStart = mid + 1
+    rightEnd = right
+    
+    tempArr = (right - left).times do ||
+      a = items[leftEnd] if leftEnd >= leftStart
+      b = items[rightEnd] if rightEnd >= rightStart
       result = nil
       result = a if b == nil
       result = b if a == nil
-      result = a if result == nil and (comparator.call(b, a) > 0)
+      result = a if result == nil and (comparator.call(a, b) > 0)
       result ||= b
       if result == a
-        left.pop
+        leftStart -= 1
       else
-        right.pop
+        rightStart -= 1
       end
       result
     end
-
-    return items
   end
 
 end
 
+size = 1000
 arr = []
-1000.times do |i|
-  arr << 10000 - i
+size.times do |i|
+  arr << size - i
 end
 ParallelMergeSort.pSort(arr, 1000)
 puts "#{arr}"

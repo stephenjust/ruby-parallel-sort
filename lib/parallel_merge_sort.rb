9b8b85725b7c33d5588a6393f38ab0b22d28de71
jesseDtucker <jesseDtucker@gmail.com>
2014-11-05 15:42:41 -0700

    And now it actually sorts.... oops

--- lib/parallel_merge_sort.rb
+++ lib/parallel_merge_sort.rb
@@ -12,48 +12,61 @@ module ParallelMergeSort
   end
 
   private
-  def self.sortItems(items, comparator, left=0, right=items.length)
+  def self.sortItems(items, comparator, left=0, right=items.length - 1)
     return unless right > left
     
     mid = (left + right) / 2
-    t1 = Thread.new do ||
-      sortItems(items, comparator, left, mid)
-    end
-    t2 = Thread.new do ||
-      sortItems(items, comparator, mid + 1, right)
-    end
-    
-    t1.join
-    t2.join
-    
     leftStart = left
     leftEnd = mid
     rightStart = mid + 1
     rightEnd = right
     
-    tempArr = (right - left).times do ||
-      a = items[leftEnd] if leftEnd >= leftStart
-      b = items[rightEnd] if rightEnd >= rightStart
+    t1 = Thread.new do ||
+      sortItems(items, comparator, leftStart, leftEnd)
+    end
+    t2 = Thread.new do ||
+      sortItems(items, comparator, rightStart, rightEnd)
+    end
+    
+    t1.join
+    t2.join
+    
+    tempArr = []
+    
+    (right - left + 1).times do ||
+      a = items[leftStart] if leftEnd >= leftStart
+      b = items[rightStart] if rightEnd >= rightStart
       result = nil
       result = a if b == nil
       result = b if a == nil
-      result = a if result == nil and (comparator.call(a, b) > 0)
+      result = a if result == nil and (comparator.call(b, a) > 0)
       result ||= b
       if result == a
-        leftStart -= 1
+        leftStart += 1
       else
-        rightStart -= 1
+        rightStart += 1
       end
-      result
+      tempArr << result
     end
+    
+    tempArr.each_with_index do |val, index|
+      items[left + index] = val
+    end    
   end
-
 end
 
-size = 1000
+size = 405
 arr = []
 size.times do |i|
-  arr << size - i
+  arr << size - i if i % 2 == 0
+  arr << size + i if i % 2 == 1
 end
+init = arr.clone
 ParallelMergeSort.pSort(arr, 1000)
-puts "#{arr}"
+
+puts "end : #{arr}"
+
+init.sort!
+init.each_with_index do |val, index|
+  puts "not sorted...." if val != arr[index]
+end
